487bc3cab8423f3e051a0c06cb42000d1a986919
Taj <tajmarie92@gmail.com>
2022-04-19 21:56:49 -0500

    Add project files.

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my fav number guessing game! Give me a number!");
+            int favNumber = 709;
+            string input = Console.ReadLine();
+            int userGuess = int.Parse(input);
+            
+            if (userGuess == favNumber)
+            {
+                Console.WriteLine("Correct!");
+
+            }
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low... Guess again");
+                
+            }
+            if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High...");
+                
+            }
+
+            input = Console.ReadLine();
+            userGuess = int.Parse(input);
+
+            if (userGuess == favNumber)
+            {
+                Console.WriteLine("Correct!");
+
+            }
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low... Guess again");
+                
+            }
+            if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High...");
+                
+            }
+             input = Console.ReadLine();
+             userGuess = int.Parse(input);
+
+            if (userGuess == favNumber)
+            {
+                Console.WriteLine("Correct!");
+
+            }
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low... Guess again");
+                
+            }
+            if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High...");
+                
+            }
+
+
+
+
+        }
+    }
+}
