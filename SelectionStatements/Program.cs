cc30bb58f1b5258a3db18ea4bf917d472b9f7dc7
Taj <tajmarie92@gmail.com>
2022-04-19 22:17:19 -0500

    Corrected code to run efficiently.

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -10,63 +10,81 @@ namespace SelectionStatements
             int favNumber = 709;
             string input = Console.ReadLine();
             int userGuess = int.Parse(input);
-            
-            if (userGuess == favNumber)
+
+            //if (userGuess == favNumber)
+            //{
+            //    Console.WriteLine("Correct!");
+
+            //}
+            //if (userGuess < favNumber)
+            //{
+            //    Console.WriteLine("Too Low... Guess again");
+
+            //}
+            //if (userGuess > favNumber)
+            //{
+            //    Console.WriteLine("Too High...");
+
+            //}
+
+            //input = Console.ReadLine();
+            //userGuess = int.Parse(input);
+
+            //if (userGuess == favNumber)
+            //{
+            //    Console.WriteLine("Correct!");
+
+            //}
+            //if (userGuess < favNumber)
+            //{
+            //    Console.WriteLine("Too Low... Guess again");
+
+            //}
+            //if (userGuess > favNumber)
+            //{
+            //    Console.WriteLine("Too High...");
+
+            //}
+            // input = Console.ReadLine();
+            // userGuess = int.Parse(input);
+
+            //if (userGuess == favNumber)
+            //{
+            //    Console.WriteLine("Correct!");
+
+            //}
+            //if (userGuess < favNumber)
+            //{
+            //    Console.WriteLine("Too Low... Guess again");
+
+            //}
+            //if (userGuess > favNumber)
+            //{
+            //    Console.WriteLine("Too High...");
+
+            //}
+
+            //Bad Way^^//
+
+            while (userGuess != favNumber)
             {
-                Console.WriteLine("Correct!");
+                if (userGuess < favNumber)
+                {
+                    Console.WriteLine("Too Low... Guess again.");
 
-            }
-            if (userGuess < favNumber)
-            {
-                Console.WriteLine("Too Low... Guess again");
-                
-            }
-            if (userGuess > favNumber)
-            {
-                Console.WriteLine("Too High...");
-                
-            }
+                }
+                if (userGuess > favNumber)
+                {
+                    Console.WriteLine("Too High... Guess again.");
 
-            input = Console.ReadLine();
-            userGuess = int.Parse(input);
+                }
 
-            if (userGuess == favNumber)
-            {
-                Console.WriteLine("Correct!");
-
-            }
-            if (userGuess < favNumber)
-            {
-                Console.WriteLine("Too Low... Guess again");
-                
-            }
-            if (userGuess > favNumber)
-            {
-                Console.WriteLine("Too High...");
-                
-            }
-             input = Console.ReadLine();
-             userGuess = int.Parse(input);
-
-            if (userGuess == favNumber)
-            {
-                Console.WriteLine("Correct!");
-
-            }
-            if (userGuess < favNumber)
-            {
-                Console.WriteLine("Too Low... Guess again");
-                
-            }
-            if (userGuess > favNumber)
-            {
-                Console.WriteLine("Too High...");
-                
+                input = Console.ReadLine();
+                userGuess = int.Parse(input);
             }
 
-
-
-
+            Console.WriteLine("Correct!");
+            //Good way ^^//
         }
     }
 }
